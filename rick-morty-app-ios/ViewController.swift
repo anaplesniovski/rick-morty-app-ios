543f65e5cceaff2995c73acc63ff5928cb8c29b2
Ana Paula Lesniovski <anap.lesniovski@gmail.com>
2023-09-28 20:55:38 -0300

    primeiro commit

---
+++ rick-morty-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-morty-app-ios
+//
+//  Created by Ana Paula Lesniovski dos Santos on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
